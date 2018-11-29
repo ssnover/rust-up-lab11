45aa86f1869e60035d329367fd03a1f939b554e4
ssnover95 <sws7379@rit.edu>
2018-11-29 14:49:54 -0500

    Move magic number for watchdog password to const mod variable

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -6,6 +6,8 @@
 extern crate msp430;
 extern crate msp430g2553;
 
+const WATCHDOG_PASSWORD: u16 = 0x5A00;
+
 /*
  * Disables the hardware watchdog timer to prevent spurious resets of the
  * microcontroller from not petting the watchdog.
@@ -14,7 +16,9 @@ pub fn disable() {
     msp430::interrupt::free(|cs| {
         let wdt = msp430g2553::WATCHDOG_TIMER.borrow(&cs);
         wdt.wdtctl.modify(|_, w| {
-            unsafe { w.bits(0x5A00) }
+            // set the password
+            unsafe { w.bits(WATCHDOG_PASSWORD) }
+            // disable the watchdog timer
             .wdthold().set_bit()
         });
     });
